a2fa29b473f60465ed638548ac3a202157602187
DohaoSTR <muzalevskij.evgenij@mail.ru>
2025-01-08 14:24:32 +0300

    build: добавил скрипт для обновления package.json данными из env файла и обновил версии библиотек.

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'; // Импорт функций для работы с URL и путями
 
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue'; // Импорт плагина Vue для Vite
+import { defineConfig } from 'vite'; // Импорт функции для определения конфигурации Vite
+import vueDevTools from 'vite-plugin-vue-devtools'; // Импорт плагина Vue DevTools для Vite
 
-// https://vite.dev/config/
+// Определение конфигурации Vite
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  // Подключение плагинов
+  plugins: [
+    vue(), // Подключение плагина Vue для Vite
+    //vueDevTools() // Подключение плагина Vue DevTools для Vite
+  ],
+
+  // Настройка разрешения путей
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // Создание псевдонима '@' для пути './src'
     },
   },
+
+  // Настройка обработки CSS
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "@/scss/_mixins.scss";
-          @import "@/scss/_variables.scss";
+          @import "@/scss/_mixins.scss"; // Импорт миксинов SCSS
+          @import "@/scss/_variables.scss"; // Импорт переменных SCSS
         `,
       },
     },
   },
-})
+
+  // Настройка сервера разработки
+  server: {
+    port: 8001, // Установка порта для сервера разработки
+    host: 'localhost', // Установка хоста для сервера разработки
+    https: false, // Отключение HTTPS для сервера разработки
+  },
+});
