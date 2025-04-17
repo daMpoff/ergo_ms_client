d6e7e88a752fbb192baf219ab109b08216d7512f
daMpoff <11projectone@gmail.com>
2025-04-17 12:40:48 +0300

    feat: Реализация двух базовых блоков для отображения

---
+++ src/pages/shortcodes/components/ButtonBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :class="buttonClass" type="button">
+    {{ component.label || 'Кнопка' }}
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  component: {
+    type: Object,
+    required: true
+  }
+})
+
+const buttonClass = computed(() => {
+  // Базовые классы + пользовательские
+  const base = 'btn'
+  const user = props.component.bootstrap_classes || ''
+  return `${base} ${user}`.trim()
+})
+</script>
---
+++ src/pages/shortcodes/components/ContainerBlock.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="finalClass">
+    <slot>{{ data.extra_data?.text || 'Контейнер' }}</slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  data: Object
+})
+
+const finalClass = computed(() => {
+  return `${props.data.bootstrap_classes} ${props.data.extra_data?.align || ''}`.trim()
+})
+</script>
+
+<style scoped>
+div {
+  min-height: 50px;
+}
+</style>
