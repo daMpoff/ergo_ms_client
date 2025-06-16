655eca4b4d9e146fe6b5db5c9ae7b03eb0276de0
daMpoff <11projectone@gmail.com>
2025-06-16 08:39:57 +0300

    feat: Добавлены компоненты, шапка, подвал

--- src/pages/shortcodes/js/index.js
+++ src/pages/shortcodes/js/index.js
@@ -1,10 +1,14 @@
 import ContainerBlock from '../components/ContainerBlock.vue'
 import ButtonBlock from '../components/ButtonBlock.vue'
 import UnknownShortcode from '../components/UnknownShortcode.vue'
+import HeaderBlock from '../components/HeaderBlock.vue'
+import FooterBlock from '../components/FooterBlock.vue'
 
 export const componentMap = {
   Container: ContainerBlock,
   Button: ButtonBlock,
+  Header: HeaderBlock,
+  Footer: FooterBlock,
 }
 
 export const defaultRenderer = UnknownShortcode
